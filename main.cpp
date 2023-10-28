e399866f4d09eec4ef3de131816db0a0d5ef8612
Pangestu <jpangestu111@gmail.com>
2023-10-28 20:00:32 +0700

    Final base version (no login)

--- main.cpp
+++ main.cpp
@@ -1,50 +1,81 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 // Defining student class
 class student{
 public:
     string name;
-    int npm;
+    string studentNumber;
+    
+    // Function to set student's score
+    string setScore(string subject, string score) {
+        // Determine grade from score
+        string grade = gradeScore(score);
 
-    string setScore(string semester, string matkul, string nilai) {
-        string x = semester1[1][0] = {matkul};
-        string y = semester1[1][1] = {nilai};
-        return x, y;
+        string x = semester1[1][0] = {subject};
+        string y = semester1[1][1] = {score};
+        string z = semester1[1][2] = {grade};
+        return x, y, z;
     }
 
-    void viewScore(string semester) {
+    // Function to view score per semester
+    void viewScore() {
+        cout << "--------------------------------------------------" << endl;
+        cout << "Name:\t\t\t" << name << endl;
+        cout << "Student Number:\t\t" << studentNumber << endl;
+        cout << "--------------------------------------------------" << endl;
+
+        // Display student's score and grade
         for(int i = 0; i < 15; i++) {
             for(int j = 0; j < 3; j++) {
-
+                // Don't print empty array
                 if(semester1[i][j] != "") {
-                    cout << semester1[i][j] << "\t";
+                    cout << semester1[i][j] << "\t\t";
                 }
             }
+            // New line for each rows
             if(semester1[i][0] != "") {
                     cout << endl;
                 }
         }
+        cout << "--------------------------------------------------" << endl;
     }
 
-    // Nilai per semester
-    string semester1[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester2[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester3[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester4[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester5[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester6[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester7[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
-    string semester8[15][3] = {"Mata Kuliah", "Nilai", "Grade"};
+private:
+    // Score per semester
+    string semester1[15][3] = {"Subject", "Score", "Grade"};
+
+    // Function to determine grades
+    string gradeScore(string score) {
+        string grade;
+        // Convert score to int so that we can use operator
+        int score_int = stoi(score);
+
+        if(score_int > 90) {
+            return grade = "A";
+        }
+        else if(score_int > 80) {
+            return grade = "B";
+        }
+        else if(score_int > 70) {
+            return grade = "C";
+        }
+        else if(score_int > 60) {
+            return grade = "D";
+        }
+        else return grade = "E";
+    }
 };
 
 
 int main() {
     int option;
     student student1;
+    student1.name = "Jesse Pinkman";
+    student1.studentNumber = "22.22.0022";
 
-    // Main menu/window
+    // Main Menu
     mainMenu:
     cout<<"----------------------------------------------------------------------"<<endl;
     cout<<"Choose one option below by typing the corresponding number (1,2,3...):"<<endl;
@@ -55,47 +86,16 @@ int main() {
     cin>>option;
     system("clear");
 
+    // If View is selected from the Main Menu
     if(option == 1) {
+        student1.viewScore();
 
-        viewMenu:
-        cout<<"----------------------------------------------------------------------"<<endl;
-        cout<<"Choose one option below by typing the corresponding number (1,2,3...):"<<endl;
-        cout<<"1. Per Semester"<<endl;
-        cout<<"2. Semua Semester"<<endl;
-        cout<<"3. Go to previous menu"<<endl;
-        cout<<"----------------------------------------------------------------------"<<endl;
-        cin>>option;
-        system("clear");
+        cout << endl;
+        cout << endl;
 
-        if(option == 1) {
-            string semester;
-            cout << "Silahkan masukkan semester: ";
-            cin >> semester;
-            
-
-            student1.viewScore(semester);
-
-            goto viewMenu;
-        }
-        else if(option == 2) {
-
-            goto viewMenu;
-            
-        }
-        else if(option == 3) {
-
-            goto mainMenu;
-            
-        }
-        else {
-        cin.ignore();
-        cout<<"Please enter the corresponding option!\nType enter to go back to the menu"<<endl;
-        cin.get();
-        system("clear");
-        goto editMenu;
-        }
-
+        goto mainMenu;
     }
+    // If Edit is selected from the Main Menu
     else if(option == 2) {
 
         editMenu:
@@ -109,17 +109,15 @@ int main() {
         system("clear");
 
         if(option == 1) {
-            string matkul, nilai, semester;
-            cout << "Silahkan masukkan semester: ";
-            cin >> semester;
-            cout << "Silahkan masukkan mata kuliah: ";
-            cin >> matkul;
-            cout << "Silahkan masukkan nilai: ";
-            cin >> nilai;
+            string subject, score;
+            cout << "Please input the subject: ";
+            cin >> subject;
+            cout << "Please input the score: ";
+            cin >> score;
             
 
-            student1.setScore(semester, matkul, nilai);
-            student1.viewScore("semester1");
+            student1.setScore(subject, score);
+            student1.viewScore();
 
             goto editMenu;
         }
@@ -153,20 +151,3 @@ int main() {
         goto mainMenu;
     }
 }
-
-
-// Create/add new student's score
-// int createScroe (string studentName, string subject) {
-
-// }
-
-
-// Read/access student's score
-
-
-
-// Update/edit student's score
-
-
-
-// Delete student's score
